1d5e064a435dfa2c8a08651c290b584994b8449c
Bruno-13 <bruno.rg.fernandes@gmail.com>
2017-04-26 20:09:52 -0400

    created and edited js files and txt

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'LjsMMML47HmwOZyHa80G4WROV',
+  consumer_secret: 'bE8DbzRADrhn5lDGL4eGsPwQNr2n6bI1JEr8NpQ9IdfLPZQFqt',
+  access_token_key: '855581468118188032-qZ1ArJz2ZJccEQ8R1TU4W5aSfcDEduR',
+  access_token_secret: 'IE5RIaUwzuAU5PHoVE96xjcaxhwqKSWkJRA0fej5oHwDK',
+}
